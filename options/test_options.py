1c4b24c5d58535a251b8c686c096517ae23818da
王亚斌 <wangyabin539@gmail.com>
2023-12-24 23:00:49 +0800

    fix test

--- options/test_options.py
+++ options/test_options.py
@@ -1,45 +1,12 @@
 from .base_options import BaseOptions
 
 
-
-from util import mkdir
-
-
-# directory to store the results
-results_dir = './results/'
-mkdir(results_dir)
-
-# root to the testsets
-# dataroot = '~/workspace/datasets/aiart/output2/sd'
-# # dataroot = '~/workspace/datasets/aiart/output2/dalle'
-# # dataroot = '~/workspace/datasets/aiart/output2/imagen'
-# # dataroot = '~/workspace/datasets/aiart/output2/mj'
-# # dataroot = '~/workspace/datasets/aiart/output2/parti'
-
-dataroot = '/home/teddy/workspace/datasets/aiart/test'
-
-# list of synthesis algorithms
-vals = ['sd','dalle','mj','parti','imagen']
-
-# indicates if corresponding testset has multiple classes
-multiclass = [0,0,0,0,0]
-
-# model
-# model_path = '/home/wangyabin/workspace/CDDDet/checkpoints/vit_sd_blur_jpg_prob0.5/model_epoch_best.pth'
-# model_path = '/home/wangyabin/workspace/CDDDet/checkpoints/vit_sd_blur_jpg_prob0.5/model_epoch_best.pth'
-# model_path = '/home/wangyabin/workspace/CDDDet/checkpoints/resnet_sd_blur_jpg_prob0.1/model_epoch_best.pth'
-# model_path = '/home/wangyabin/workspace/CDDDet/checkpoints/resnet_sd_blur_jpg_prob0.5/model_epoch_best.pth'
-# model_path = '/home/wangyabin/workspace/CDDDet/weights/blur_jpg_prob0.1.pth'
-# model_path = '/home/wangyabin/workspace/CDDDet/weights/blur_jpg_prob0.5.pth'
-model_path = '/home/teddy/workspace/CDDDet/checkpoints/vit_sdft_set3/model_epoch_latest.pth'
-
-
-
-
 class TestOptions(BaseOptions):
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)
         parser.add_argument('--model_path')
+        parser.add_argument('--results_dir')
+        parser.add_argument('--data_root')
         parser.add_argument('--no_resize', action='store_true')
         parser.add_argument('--no_crop', action='store_true')
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
--- test.py
+++ test.py
@@ -5,44 +5,47 @@ import torch.nn as nn
 import timm
 
 from utils.validate import validate
-
 from options.test_options import TestOptions
 
 
-# Running tests
-opt = TestOptions().parse(print_options=False)
-model_name = os.path.basename(model_path).replace('.pth', '')
-rows = [["{} model testing on...".format(model_name)],
-        ['testset', 'accuracy', 'avg precision']]
+if __name__ == '__main__':
 
-print("{} model testing on...".format(model_name))
-for v_id, val in enumerate(vals):
-    opt.dataroot = '{}/{}'.format(dataroot, val)
-
-    opt.isTrain = False
-    opt.no_resize = False
-    opt.no_crop = False
-    opt.serial_batches = True
-    opt.jpg_method = ['pil']
-
-    print(opt.dataroot)
-    opt.classes = os.listdir(opt.dataroot) if multiclass[v_id] else ['']
-    # opt.no_resize = True    # testing without resizing by default
-
-    model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=1)
-    # model = resnet50(num_classes=1)
-    state_dict = torch.load(model_path, map_location='cpu')
-    model.load_state_dict(state_dict['model'])
-    model.cuda()
-    model.eval()
-
-    acc, ap, _, _, _, _ = validate(model, opt)
-    rows.append([val, acc, ap])
-    print("({}) acc: {}; ap: {}".format(val, acc, ap))
+    # Running tests
+    opt = TestOptions().parse(print_options=False)
 
 
 
-csv_name = results_dir + '/{}.csv'.format(model_name)
-with open(csv_name, 'w') as f:
-    csv_writer = csv.writer(f, delimiter=',')
-    csv_writer.writerows(rows)
+
+    model_name = os.path.basename(model_path).replace('.pth', '')
+    rows = [["{} model testing on...".format(model_name)],
+            ['testset', 'accuracy', 'avg precision']]
+
+    print("{} model testing on...".format(model_name))
+    for v_id, val in enumerate(vals):
+        opt.dataroot = '{}/{}'.format(dataroot, val)
+
+        opt.isTrain = False
+        opt.no_resize = False
+        opt.no_crop = False
+        opt.serial_batches = True
+        opt.jpg_method = ['pil']
+
+        print(opt.dataroot)
+        opt.classes = os.listdir(opt.dataroot) if multiclass[v_id] else ['']
+        # opt.no_resize = True    # testing without resizing by default
+
+        model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=1)
+        # model = resnet50(num_classes=1)
+        state_dict = torch.load(model_path, map_location='cpu')
+        model.load_state_dict(state_dict['model'])
+        model.cuda()
+        model.eval()
+
+        acc, ap, _, _, _, _ = validate(model, opt)
+        rows.append([val, acc, ap])
+        print("({}) acc: {}; ap: {}".format(val, acc, ap))
+
+    csv_name = results_dir + '/{}.csv'.format(model_name)
+    with open(csv_name, 'w') as f:
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerows(rows)
