07ce896017c280c782a0529b07425b123e2958a7
王亚斌 <wangyabin539@gmail.com>
2023-12-24 08:26:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import torch
 import torch.nn as nn
 import timm
 
-from validate import validate
-from networks.resnet import resnet50
+from utils.validate import validate
+
 from options.test_options import TestOptions
 
 
@@ -24,25 +24,12 @@ for v_id, val in enumerate(vals):
     opt.no_crop = False
     opt.serial_batches = True
     opt.jpg_method = ['pil']
-    # if len(opt.blur_sig) == 2:
-    #     b_sig = opt.blur_sig
-    #     opt.blur_sig = [(b_sig[0] + b_sig[1]) / 2]
-    # if len(opt.jpg_qual) != 1:
-    #     j_qual = opt.jpg_qual
-    #     opt.jpg_qual = [int((j_qual[0] + j_qual[-1]) / 2)]
-    print(opt)
-    # opt.isTrain = False
-    # opt.no_resize = False
-    # opt.no_crop = False
-    # opt.serial_batches = True
-    # print(opt)
 
     print(opt.dataroot)
     opt.classes = os.listdir(opt.dataroot) if multiclass[v_id] else ['']
     # opt.no_resize = True    # testing without resizing by default
 
     model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=1)
-
     # model = resnet50(num_classes=1)
     state_dict = torch.load(model_path, map_location='cpu')
     model.load_state_dict(state_dict['model'])
