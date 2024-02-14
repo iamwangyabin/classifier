12d9c00460deb2f0e5f5bf95d1ae86993f05b3b2
王亚斌 <wangyabin539@gmail.com>
2024-02-14 17:00:38 +0800

    add test

---
+++ test.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test_CNNSpot0.1_224.yaml
+
+CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test_CNNSpot0.5_224.yaml
+
+CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test_CNNSpot0.1_noresize.yaml
+
+CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test_CNNSpot0.5_noresize.yaml
