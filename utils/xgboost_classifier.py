3292e3b7aac178524c569a6d1430b842c010c5ad
王亚斌 <wangyabin539@gmail.com>
2024-04-02 01:55:03 +0800

    update test

--- utils/xgboost_classifier.py
+++ utils/xgboost_classifier.py
@@ -1,8 +1,8 @@
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, average_precision_score
 import numpy as np
 import pickle
 import xgboost as xgb
+import csv
 
 with open('clip_features_test.pkl', 'rb') as file:
     loaded_test_dict = pickle.load(file)
@@ -10,57 +10,19 @@ with open('clip_features_test.pkl', 'rb') as file:
 with open('clip_progan_train_features.pkl', 'rb') as file:
     loaded_train_dict = pickle.load(file)
 
-# session_names = [key for key in loaded_test_dict.keys() if not key.startswith('AntifakePrompt')]
 train_features = loaded_train_dict['ProGAN_train'][0]
 train_labels = loaded_train_dict['ProGAN_train'][1]
-# svm_classifier = SVC(kernel='rbf')
-# svm_classifier.fit(train_features, train_labels)
-# y_pred = svm_classifier.predict(X_test)
-# print(classification_report(y_test, y_pred))
-# print("Accuracy:", accuracy_score(y_test, y_pred))
 
-
-scale_pos_weight = sum(train_labels == 0) / sum(train_labels == 1)
-xgb_classifier = xgb.XGBClassifier(objective='binary:logistic', scale_pos_weight=scale_pos_weight,
-                                   use_label_encoder=False, eval_metric='logloss', verbose_eval=True)
+# scale_pos_weight = sum(train_labels == 0) / sum(train_labels == 1)        scale_pos_weight=scale_pos_weight,
+xgb_classifier = xgb.XGBClassifier(objective='binary:logistic', use_label_encoder=False, eval_metric='logloss',
+                                   verbose_eval=True,
+                                   n_estimators=50,
+                                   learning_rate=0.1,
+                                   max_depth=10,
+                                   )
 
 xgb_classifier.fit(train_features, train_labels)
 
-from xgboost import plot_importance
-
-fig, ax = plt.subplots(figsize=(10, 15))
-plot_importance(xgb_classifier, ax=ax, max_num_features=20)
-plt.show()
-
-
-
-booster = xgb_classifier.get_booster()
-importance = booster.get_score(importance_type='weight')
-sorted_features = sorted(importance.items())
-features, importances = zip(*sorted_features)
-plt.figure(figsize=(10, 6))
-plt.bar(range(len(importances)), importances, align='center')
-plt.title('Feature Importances')
-plt.xlabel('Features')
-plt.ylabel('Importance Score')
-plt.xticks([])
-plt.show()
-
-
-# # 在测试集上进行预测
-# y_pred = xgb_classifier.predict(X_test)
-# y_pred_proba = xgb_classifier.predict_proba(X_test)[:, 1]
-
-# # 评估分类器的性能
-# accuracy = accuracy_score(y_test, y_pred)
-# roc_auc = roc_auc_score(y_test, y_pred_proba)
-# print(f"Model accuracy: {accuracy:.2f}")
-# print(f"ROC AUC score: {roc_auc:.2f}")
-
-
-
-
-from sklearn.metrics import accuracy_score, average_precision_score
 
 session_names = [key for key in loaded_test_dict.keys()]
 save_raw_results = []
@@ -78,10 +40,6 @@ for session_name in session_names:
     print([ap, r_acc, f_acc, acc])
 
 
-
-
-import csv
-
 columns = ['dataset', 'sub_set', 'ap', 'r_acc0', 'f_acc0', 'acc0']
 with open('sgboost_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
@@ -91,6 +49,58 @@ with open('sgboost_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
 
 
 
+
+
+# 画出来特征重要性图
+from xgboost import plot_importance
+
+fig, ax = plt.subplots(figsize=(10, 15))
+plot_importance(xgb_classifier, ax=ax, max_num_features=20)
+plt.show()
+
+booster = xgb_classifier.get_booster()
+importance = booster.get_score(importance_type='weight')
+sorted_features = sorted(importance.items())
+features, importances = zip(*sorted_features)
+plt.figure(figsize=(10, 6))
+plt.bar(range(len(importances)), importances, align='center')
+plt.title('Feature Importances')
+plt.xlabel('Features')
+plt.ylabel('Importance Score')
+plt.xticks([])
+plt.show()
+
+
+
+
+
+
+
+# 在LSUN的20类上训练一个xgboost
+
+
+
+with open('clip_progan_train_multicls_real_features.pkl', 'rb') as file:
+    loaded_train_dict = pickle.load(file)
+
+
+train_features = loaded_train_dict['ProGAN_train_real'][0]
+train_labels = loaded_train_dict['ProGAN_train_real'][1]
+
+xgb_classifier = xgb.XGBClassifier(objective='multi:softprob', num_class=20, use_label_encoder=False, verbose_eval=True)
+
+xgb_classifier.fit(train_features, train_labels)
+
+
+
+with open('clip_progan_train_multicls_fake_features.pkl', 'rb') as file:
+    loaded_test_dict = pickle.load(file)
+
+
+test_features = loaded_test_dict['ProGAN_train_fake'][0]
+test_labels = loaded_test_dict['ProGAN_train_fake'][1]
+
+y_pred = xgb_classifier.predict_proba(test_features)
 
 
 
