f363a8328883ce909987805ec8c3853573339571
王亚斌 <wangyabin539@gmail.com>
2024-04-02 18:30:19 +0800

    Update detector.py

--- networks/ViTDetector/detector.py
+++ networks/ViTDetector/detector.py
@@ -7,11 +7,11 @@ import timm
 class ViTModel(nn.Module):
     def __init__(self, name, num_classes=1):
         super(ViTModel, self).__init__()
-        self.backbones = timm.create_model(name, pretrained=True)
+        self.backbone = timm.create_model(name, pretrained=True)
         self.backbone.eval()
         for param in self.backbone.parameters():
             param.requires_grad = False
-        self.fc = nn.Linear(self.backbones.num_features, num_classes)
+        self.fc = nn.Linear(self.backbone.num_features, num_classes)
         torch.nn.init.xavier_uniform_(self.fc.weight.data)
 
     def forward(self, x, return_feature=False):
