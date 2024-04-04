a33728c8989d454155995487cec94e44a3f9855f
王亚斌 <wangyabin539@gmail.com>
2024-04-05 01:11:33 +0800

    fix bug zsclip

--- networks/SPrompts/zsclip.py
+++ networks/SPrompts/zsclip.py
@@ -1,49 +1,29 @@
 import torch
 import torch.nn as nn
 
-from dassl.engine import TRAINER_REGISTRY, TrainerX
-from dassl.optim import build_optimizer, build_lr_scheduler
-
 from clip import clip
 from clip.model import convert_weights
 
 from .coop import load_clip_to_cpu
 from .imagenet_templates import IMAGENET_TEMPLATES, IMAGENET_TEMPLATES_SELECT
 
-CUSTOM_TEMPLATES = {
-    "OxfordPets": "a photo of a {}, a type of pet.",
-    "OxfordFlowers": "a photo of a {}, a type of flower.",
-    "FGVCAircraft": "a photo of a {}, a type of aircraft.",
-    "DescribableTextures": "{} texture.",
-    "EuroSAT": "a centered satellite photo of {}.",
-    "StanfordCars": "a photo of a {}.",
-    "Food101": "a photo of {}, a type of food.",
-    "SUN397": "a photo of a {}.",
-    "Caltech101": "a photo of a {}.",
-    "UCF101": "a photo of a person doing {}.",
-    "ImageNet": "a photo of a {}.",
-    "ImageNetSketch": "a photo of a {}.",
-    "ImageNetV2": "a photo of a {}.",
-    "ImageNetA": "a photo of a {}.",
-    "ImageNetR": "a photo of a {}.",
-}
 
-
-@TRAINER_REGISTRY.register()
-class ZeroshotCLIP(TrainerX):
-    def build_model(self):
+class ZeroshotCLIP(nn.Module):
+    def __init__(self, cfg, classnames, device):
+        super().__init__()
         cfg = self.cfg
-        classnames = self.dm.dataset.classnames
+        n_cls = len(classnames)
 
         print(f"Loading CLIP (backbone: {cfg.MODEL.BACKBONE.NAME})")
         clip_model = load_clip_to_cpu(cfg)
-        clip_model.to(self.device)
-
-        temp = CUSTOM_TEMPLATES[cfg.DATASET.NAME]
+        clip_model.to(device)
+        for params in clip_model.parameters():
+            params.requires_grad_(False)
+        temp = "a photo of a {}."
         prompts = [temp.format(c.replace("_", " ")) for c in classnames]
         print(f"Prompts: {prompts}")
         prompts = torch.cat([clip.tokenize(p) for p in prompts])
-        prompts = prompts.to(self.device)
+        prompts = prompts.to(device)
 
         with torch.no_grad():
             text_features = clip_model.encode_text(prompts)
@@ -52,7 +32,7 @@ class ZeroshotCLIP(TrainerX):
         self.text_features = text_features
         self.clip_model = clip_model
 
-    def model_inference(self, image):
+    def forward(self):
         image_features = self.clip_model.encode_image(image)
         image_features = image_features / image_features.norm(dim=-1, keepdim=True)
         logit_scale = self.clip_model.logit_scale.exp()
@@ -60,35 +40,34 @@ class ZeroshotCLIP(TrainerX):
         return logits
 
 
-@TRAINER_REGISTRY.register()
-class ZeroshotCLIP2(ZeroshotCLIP):
+
+class ZeroshotCLIP_PE(nn.Module):
     """Prompt ensembling."""
 
     # templates = IMAGENET_TEMPLATES
     templates = IMAGENET_TEMPLATES_SELECT
 
-    def build_model(self):
+
+    def __init__(self, cfg, classnames, device):
+        super().__init__()
         cfg = self.cfg
-        classnames = self.dm.dataset.classnames
+        n_cls = len(classnames)
 
         print(f"Loading CLIP (backbone: {cfg.MODEL.BACKBONE.NAME})")
         clip_model = load_clip_to_cpu(cfg)
-        clip_model.to(self.device)
+        clip_model.to(device)
 
         for params in clip_model.parameters():
             params.requires_grad_(False)
 
-        # add custom-made prompt
-        if cfg.DATASET.NAME != "ImageNet":
-            self.templates += [CUSTOM_TEMPLATES[cfg.DATASET.NAME]]
-
+        self.templates +=  "a photo of a {}."
         num_temp = len(self.templates)
         print(f"Prompt ensembling (n={num_temp})")
 
         mean_text_features = 0
         for i, temp in enumerate(self.templates):
             prompts = [temp.format(c.replace("_", " ")) for c in classnames]
-            prompts = torch.cat([clip.tokenize(p) for p in prompts]).to(self.device)
+            prompts = torch.cat([clip.tokenize(p) for p in prompts]).to(device)
             text_features = clip_model.encode_text(prompts)
             text_features = text_features / text_features.norm(dim=-1, keepdim=True)
             mean_text_features = mean_text_features + text_features
@@ -97,3 +76,15 @@ class ZeroshotCLIP2(ZeroshotCLIP):
 
         self.text_features = mean_text_features
         self.clip_model = clip_model
+
+    def forward(self):
+        image_features = self.clip_model.encode_image(image)
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        logit_scale = self.clip_model.logit_scale.exp()
+        logits = logit_scale * image_features @ self.text_features.t()
+        return logits
+
+
+
+
+
