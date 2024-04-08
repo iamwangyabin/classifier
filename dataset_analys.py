e8b4072fa9b62cc4c2647e5c7df8f6c36fa16f39
王亚斌 <wangyabin539@gmail.com>
2024-04-08 21:59:08 +0800

    update

--- dataset_analys.py
+++ dataset_analys.py
@@ -1,18 +1,69 @@
-import argparse
-from utils.util import load_config_with_cli
+# plz install https://github.com/marcojira/fld.git
+
 import os
-import hydra
 import json
-from accelerate import Accelerator
+import hydra
+import argparse
 from PIL import ImageFile, Image
+from utils.util import load_config_with_cli
 
 import torch
-from torchmetrics.image.fid import FrechetInceptionDistance
-from torchmetrics.image.inception import InceptionScore
-from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from torch.utils.data import Dataset
+import torch.nn.functional as F
 
-from utils.metrics.clip_score import CLIPIScore as CLIP_IScore
-from utils.metrics.dino_score import DINOScore as DINO_Score
+from fld.features.DINOv2FeatureExtractor import DINOv2FeatureExtractor
+from fld.features.CLIPFeatureExtractor import CLIPFeatureExtractor
+from fld.features.InceptionFeatureExtractor import InceptionFeatureExtractor
+from fld.metrics.FID import FID
+from fld.metrics.FLD import FLD
+from fld.metrics.KID import KID
+from fld.metrics.PrecisionRecall import PrecisionRecall
+
+
+class JsonDatasets(Dataset):
+    def __init__(self, opt, data_root, subset='all', split='train', selected_label=0):
+        self.dataroot = data_root
+        self.split = split
+        self.image_pathes = []
+        self.labels = []
+
+        json_file = os.path.join(self.dataroot, f'{split}.json')
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+
+        for img_rel_path, label in data[subset].items():
+            img_full_path = os.path.join(self.dataroot, img_rel_path)
+            if label == selected_label:
+                self.image_pathes.append(img_full_path)
+                self.labels.append(label)
+
+    def __len__(self):
+        return len(self.image_pathes)
+
+    def __getitem__(self, idx):
+        img_path = self.image_pathes[idx]
+        image = Image.open(img_path).convert('RGB')
+        label = self.labels[idx]
+        return image, label
+
+
+def fisher_score(feature1, feature2):
+    mean_A = torch.mean(feature1, dim=0)
+    mean_B = torch.mean(feature2, dim=0)
+
+    def within_class_scatter(feat, mean):
+        diff = feat - mean
+        scatter = diff.T @ diff
+        return scatter
+
+    scatter_A = within_class_scatter(feature1, mean_A)
+    scatter_B = within_class_scatter(feature2, mean_B)
+    S_W = scatter_A + scatter_B
+    mean_diff = (mean_A - mean_B).reshape(-1, 1)
+    S_B = mean_diff @ mean_diff.T
+    S_W_inv = torch.pinverse(S_W)
+    w = S_W_inv @ mean_diff
+    return w
 
 
 if __name__ == '__main__':
@@ -25,45 +76,66 @@ if __name__ == '__main__':
     all_results = []
     for set_name, source_conf in conf.datasets.source.items():
         data_root = source_conf.data_root
+        json_file = os.path.join(data_root, 'test.json')
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+
         for subset in source_conf.sub_sets:
-            json_file = os.path.join(source_conf.data_root, 'test.json')
-            with open(json_file, 'r') as f:
-                data = json.load(f)
-
-            accelerator = Accelerator()
-
-            dino_score = DINO_Score(model_name_or_path='dino_vits16')
-            clip_i_score = CLIP_IScore(model_name_or_path='openai/clip-vit-base-patch32')
-            fid = FrechetInceptionDistance(feature=2048)
-            inception = InceptionScore()
-
-            dino_score = accelerator.prepare_model(dino_score, evaluation_mode=True)
-            clip_i_score = accelerator.prepare_model(clip_i_score, evaluation_mode=True)
-            fid = accelerator.prepare_model(fid, evaluation_mode=True)
-            inception = accelerator.prepare_model(inception, evaluation_mode=True)
-
-
-            fake_images, real_images = [], []
-            for img_rel_path, label in data[subset].items():
-                img_full_path = os.path.join(source_conf.data_root, img_rel_path)
-                if label == 1:
-                    fake_images.append(Image.open(img_full_path).convert('RGB'))
-                else:
-                    real_images.append(Image.open(img_full_path).convert('RGB'))
-
-
-            dino_score.update(fake_images, real_images)
-            clip_i_score.update(fake_images, real_images)
-            fid.update(real_images, real=True)
-            fid.update(fake_images, real=False)
-            inception.update(fake_images)
-
             print(f"{set_name} {subset}")
-            all_results.append([set_name, subset, (dino_score.compute()).item(), (clip_i_score.compute()).item(),
-                                (fid.compute()).item(), (inception.compute()).item()])
+            clip_feature_extractor = CLIPFeatureExtractor()
+            dino_feature_extractor = DINOv2FeatureExtractor()
+            inception_feature_extractor = InceptionFeatureExtractor()
+
+            gen_dataset = JsonDatasets(conf, data_root, subset, split='test', selected_label=1)
+            real_dataset = JsonDatasets(conf, data_root, subset, split='test', selected_label=0)
+
+            clip_gen_feat = clip_feature_extractor.get_features(gen_dataset)
+            clip_real_feat = clip_feature_extractor.get_features(real_dataset)
+
+            # import pdb;pdb.set_trace()
+            dino_gen_feat = dino_feature_extractor.get_features(gen_dataset)
+            dino_real_feat = dino_feature_extractor.get_features(real_dataset)
+            # inception_gen_feat = inception_feature_extractor.get_features(gen_dataset)
+            # inception_real_feat = inception_feature_extractor.get_features(real_dataset)
+
+            clip_fid = FID().compute_metric(clip_real_feat, None, clip_gen_feat)
+            clip_kid = KID().compute_metric(clip_real_feat, None, clip_gen_feat)
+            clip_precision = PrecisionRecall(mode="Precision").compute_metric(clip_real_feat, None, clip_gen_feat)
+            clip_reacall = PrecisionRecall(mode="Recall", num_neighbors=5).compute_metric(clip_real_feat, None, clip_gen_feat)
+
+            dino_fid = FID().compute_metric(dino_real_feat, None, dino_gen_feat)
+            dino_kid = KID().compute_metric(dino_real_feat, None, dino_gen_feat)
+            dino_precision = PrecisionRecall(mode="Precision").compute_metric(dino_real_feat, None, dino_gen_feat)
+            dino_reacall = PrecisionRecall(mode="Recall", num_neighbors=5).compute_metric(dino_real_feat, None, dino_gen_feat)
 
 
-            import pdb;pdb.set_trace()
+            minsize = min(clip_real_feat.size(0), clip_gen_feat.size(0), dino_real_feat.size(0), dino_gen_feat.size(0))
+            clip_score = F.cosine_similarity(clip_real_feat[:minsize, :], clip_gen_feat[:minsize, :], dim=1).mean()
+            dino_score = F.cosine_similarity(dino_real_feat[:minsize, :], dino_gen_feat[:minsize, :], dim=1).mean()
+
+            # fisher_score(clip_gen_feat, clip_real_feat)
+
+            all_results.append([set_name, subset, clip_fid, clip_kid, clip_precision, clip_reacall,
+                                dino_fid, dino_kid, dino_precision, dino_reacall, clip_score, dino_score])
+            # inception_fid = FID().compute_metric(inception_real_feat, None, inception_gen_feat)
+            # inception_kid = KID().compute_metric(inception_real_feat, None, inception_gen_feat)
+            # inception_precision = PrecisionRecall(mode="Precision").compute_metric(inception_real_feat, None, inception_gen_feat)
+            # inception_reacall = PrecisionRecall(mode="Recall", num_neighbors=5).compute_metric(inception_real_feat, None, inception_gen_feat)
+
+            # score = 100 * (clip_real_feat * clip_gen_feat).sum(axis=-1)
+
+        columns = ['dataset', 'sub_set', 'clip_fid', 'clip_kid', 'clip_precision', 'clip_reacall',
+                   'dino_fid', 'dino_kid', 'dino_precision', 'dino_reacall',
+                   'clip_score', 'dino_score']
+        with open('dataset_analysis_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(columns)
+            for values in all_results:
+                writer.writerow(values)
+
+
+
+
 
 
 
