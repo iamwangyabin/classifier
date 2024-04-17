141544e3de88c9ba3904c727f0a0f0c8f5ee4317
王亚斌 <wangyabin539@gmail.com>
2024-04-17 17:54:08 +0800

    update

--- train2.sh
+++ train2.sh
@@ -28,6 +28,6 @@ conda activate timm
 
 #python train.py --cfg cfgs/train_clip_sd15.yaml
 
-#python train.py --cfg cfgs/train/train_arp_progan.yaml
+python train.py --cfg cfgs/train/train_arp_progan.yaml
 
-python train.py --cfg cfgs/train/train_arp_progan2.yaml
+#python train.py --cfg cfgs/train/train_arp_progan2.yaml
