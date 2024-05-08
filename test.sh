67edf42b46910899ec314de55704e7d2418b9d78
王亚斌 <wangyabin539@gmail.com>
2024-05-09 02:45:24 +0800

    more

--- test.sh
+++ test.sh
@@ -36,36 +36,35 @@ conda activate timm
 #
 #python test.py --cfg cfgs/test/test_arp2_jpg90_224.yaml
 
-#python train.py --cfg cfgs/train/train_arp_progan.yaml
-#
-#python train.py --cfg cfgs/train/train_arp_progan2.yaml
-#
-#python train.py --cfg cfgs/train/train_arp_progan3.yaml
-#
-#python train.py --cfg cfgs/train/train_arp_progan4.yaml
-#
-#python train.py --cfg cfgs/train/train_arp_progan5.yaml
-#
-#python train.py --cfg cfgs/train/train_arp_progan6.yaml
-#
-#python train.py --cfg cfgs/train/train_arp_progan7.yaml
-#
-#
+python train.py --cfg cfgs/train/train_arp_progan.yaml
+
+python train.py --cfg cfgs/train/train_arp_progan2.yaml
+
+python train.py --cfg cfgs/train/train_arp_progan3.yaml
+
+python train.py --cfg cfgs/train/train_arp_progan4.yaml
+
+python train.py --cfg cfgs/train/train_arp_progan5.yaml
+
+python train.py --cfg cfgs/train/train_arp_progan6.yaml
+
+python train.py --cfg cfgs/train/train_arp_progan7.yaml
+
 #python test_fusing.py --cfg cfgs/test/test_Fusing_jpeg90_224.yaml
-#
+
 ##python test_lgrad.py --cfg cfgs/test/test_LGrad_jpeg90_224.yaml
-#
+
 #python test_lnp.py --cfg cfgs/test/test_LNP_jpeg90_224.yaml
 
 
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f_6.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_j90.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_j90_2.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f_3.yaml
 
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_j90_4.yaml
@@ -73,22 +72,20 @@ conda activate timm
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f_5.yaml
 
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f_4.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f_2.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_f_7.yaml
-#
+
 #CUDA_VISIBLE_DEVICES=2 python test.py --cfg cfgs/test/a_j90_3.yaml
-
-
-python test.py --cfg cfgs/test/a_f.yaml
-python test.py --cfg cfgs/test/a_f_2.yaml
-python test.py --cfg cfgs/test/a_f_3.yaml
-python test.py --cfg cfgs/test/a_f_4.yaml
-python test.py --cfg cfgs/test/a_j90.yaml
-python test.py --cfg cfgs/test/a_j90_2.yaml
-python test.py --cfg cfgs/test/a_j90_3.yaml
-python test.py --cfg cfgs/test/a_j90_4.yaml
+#python test.py --cfg cfgs/test/a_f.yaml
+#python test.py --cfg cfgs/test/a_f_2.yaml
+#python test.py --cfg cfgs/test/a_f_3.yaml
+#python test.py --cfg cfgs/test/a_f_4.yaml
+#python test.py --cfg cfgs/test/a_j90.yaml
+#python test.py --cfg cfgs/test/a_j90_2.yaml
+#python test.py --cfg cfgs/test/a_j90_3.yaml
+#python test.py --cfg cfgs/test/a_j90_4.yaml
 
 
 
