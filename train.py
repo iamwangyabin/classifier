6af7f3d2e90a253397bc034bf53a006524b3393d
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2024-06-12 16:30:43 +0100

    update

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
         monitor='val_acc_epoch',
         dirpath=os.path.join('logs', today_str),
         filename='{epoch:02d}-{val_acc_epoch:.2f}',
-        save_top_k=1,
+        save_top_k=3,
         mode='max',
     )
 
